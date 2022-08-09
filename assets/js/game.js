8beae845e7cfe086c22f2392894dd113ccb54a08
Brian Lockhart <109180146+bslockhart@users.noreply.github.com>
2022-08-09 01:05:49 -0400

    Updated 3.1.6:

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,31 +1,38 @@
-//* Introducing Functions.
-//* The alert() function tells the browser to display a message (i.e., an alert) to the
-//* user.
 //window.alert("This is an alert! JavaScript is running!");
 
-
-//* This creates a function named "fight".
-//* Placing content between the parentheses is called passing an argument into a function.
-//* Declaring a Function: Giving it a name.
-//function fight() {
 //window.alert("The fight has begun!");
-//}
-//* Calling a Function.
 //fight();
 
-//* Storing User Input Using Variables: Prompt's.
 //window.prompt("What is your robot's name?");
-//* We need to capture this data somehow. To do that, we'll add an expression to the JS file.
-//* Introducing Variables.
 //var playerName = window.prompt("What is your robot's name?");
 
-//* The Console: A Developer's Best Friend.
+//var playerName = window.prompt("What is your robot's name?");
+//console.log(playerName);
+
+//console.log("This logs a string, good for leaving yourself a message");
+//console.log(10 + 10);
+//console.log("Our robot's name is " + playerName);
+
+//* 3.1.6
 var playerName = window.prompt("What is your robot's name?");
-//* Call the function.
-console.log(playerName);
-//* This is a string, good for leaving yourself a message.
-console.log("This logs a string, good for leaving yourself a message");
-//* This log does math.
-console.log(10 + 10);
-//* What is this?
-console.log("Our robot's name is " + playerName);
+var playerHealth = 100;
+var playerAttack = 10;
+
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+  window.alert("Welcome to Robot Gladiators!");
+};
+enemyHealth = enemyHealth - playerAttack;
+playerHealth = playerHealth - enemyAttack;
+console.log(
+  //playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+  enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+  );
+
+
+fight();
